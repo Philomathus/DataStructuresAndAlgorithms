535fb70bcb4b6f4849fb99b54ff8d42c8e9e7907
Maxwell <philomath241199@gmail.com>
2022-09-20 19:33:25 +0800

    Add

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,13 +1,27 @@
 package com.main;
 
 import com.main.list.SinglyLinkedList;
-
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+//        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+//
+//        int[] arr1 = {1,2,3};
+//        int[] arr2 = {1,2,3};
+//
+//        System.out.println(Arrays.equals(arr1, arr2));
+//        Arrays.fill(arr1, 0);
+//        String arr1String = Arrays.toString(arr1);
+//        System.out.println(arr1);
+//
+//        System.out.println(Arrays.toString(arr1));
+
+        String str = String.format("%02d", 98);
+
+        System.out.printf("%02d", 98);
+
 
 
     }
